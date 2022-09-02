950a44e3ce8252c3f78c0cfb6fc8398499789411
addis-hail <addishailegnaw@yahoo.com>
2022-09-02 09:33:33 +0300

    adding content using jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
-$("#contact-methods").css({border: "2px solid red"})
-.next().css({border: "2px solid green"})
-.closest("section").css({border: "2px solid blue"});
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham Vs Cheese: </div>";
+
+// $("#tweets div").append(tweet);
+// $("#tweets div").prepend(tweet);
+// $("#tweets div").before(tweet);
+// $("#tweets div p").after(tweet);
+$("#tweets div").html(tweet);
+// $("#tweets div p").text(tweet);
