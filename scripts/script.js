ad70f90fc2b6deff9766fbd5e96b256ef795dfc5
addis-hail <addishailegnaw@yahoo.com>
2022-09-05 13:41:00 +0300

    event helpers

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
-var myLis = $("#points-of-sale li");
-
-myLis.on("click", function(){
-    $(this).css({"background" : "pink"});
-    myLis.off("click");
+// $("#lead-banner").click(function(){
+//     alert("you clicked me");
+// });
+// $("#lead-banner").dblclick(function(){
+//     alert("you  double clicked me");
+// });
+// $("#lead-banner").on("dblclick", function(){
+//     alert("you  double clicked me");
+// });
+$("#lead-banner").dblclick(function(){
+    alert("you  double clicked me");
+    $("#lead-banner").off("dblclick");
 });
