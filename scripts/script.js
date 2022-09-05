e57ceba5e094822a9a9fea3a16fc53ad27776284
addis-hail <addishailegnaw@yahoo.com>
2022-09-05 19:07:42 +0300

    event object in jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// });
+    $("*").on("click", function(e){
 
-
-$(function(){
+        console.log(e.target);
+        console.log("The event type is: " + e.type)
+        console.log("X co-ordinate of the event is: " + e.pageX);
+        console.log("Y co-ordinate of the event is: " + e.pageY);
+        e.stopPropagation();
+    });
 
 });
-
-// $(window).load(function(){
-
-// });
