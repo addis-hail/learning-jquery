38537df394ebe4a52e1534d32b248491b843a63e
addis-hail <addishailegnaw@yahoo.com>
2022-09-08 14:42:44 +0300

    fading animation example-1

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
-    // $(".slide-button-up").on("click", function(){
-    //     $("#lead-banner").slideUp(100);
-    // });
+    var allQuotes = $("blockquote");
+    var currentQuote = 0;
 
-    // $(".slide-button-down").on("click", function(){
-    //     $("#lead-banner").slideDown(4000, function(){
-    //         alert("animation complete")
-    //     });
-    // });
-
-    $(".slide-button-up").on("click", function(){
-        $("#lead-banner").slideToggle(1000, function(){
-                alert("animation complete")
+    function changeQuote(){
+        $(allQuotes[currentQuote]).fadeOut(200, function(){
+            if(currentQuote = allQuotes.length-1){
+                currentQuote = 0;
+            }else{
+                currentQuote++;
+            }
         });
-    });
+
+        $(allQuotes[currentQuote]).fadeIn(200);
+    }
+    var quoteTimer = setInterval(changeQuote, 3000);
 
 });
