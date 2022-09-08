4007e5b93aab1bb2ccc1baa8d1c17c08350d059f
addis-hail <addishailegnaw@yahoo.com>
2022-09-08 10:33:44 +0300

    hide and show elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
 
-    $("section > h2").on("click", function(){
-        // $(this).fadeOut(2000).fadeIn(500);
-        $(this).fadeTo(200, 0.2)
-               .fadeTo(200, 0.8)
-               .fadeTo(200, 0.2)
-               .fadeTo(200, 0.8)
-               .fadeTo(200, 0.2)
-               .fadeTo(200, 0.8);
+    // $("section > h2").on("click", function(){
+    //     $(this).hide(1000).show(1000);
+    // });
+
+    $("img[alt=map]").on("click", function(){
+        $("section > h2").toggle(1000);
     });
 
 });
